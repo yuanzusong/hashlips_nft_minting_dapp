ab5b5fd0861af050f170387e38adc3106a313f96
tuplesong <tuplesong@gmail.com>
2022-06-12 21:28:22 +0800

    add new files

--- src/App.js
+++ src/App.js
@@ -162,8 +162,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 10) {
-      newMintAmount = 10;
+    if (newMintAmount > 2) {
+      newMintAmount = 2;
     }
     setMintAmount(newMintAmount);
   };
